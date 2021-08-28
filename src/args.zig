f2832c925c9060de0855e480bfc3d29ee3f04f97
Lion Kortlepel <development@kortlepel.com>
2021-08-28 13:06:27 +0300

    finished args.zig library 1.0

--- src/args.zig
+++ src/args.zig
@@ -1,8 +1,5 @@
 const std = @import("std");
-const stdalloc = std.heap.page_allocator;
 
-/// zero-length key is invalid,
-/// zero-length value is valid for no-value parameters ('flags')
 const Argument = struct {
     isShort: bool,
     key: union {
@@ -17,15 +14,17 @@ const ParseArgumentsError = error{
     InvalidArgument,
 };
 
-fn parseArguments() !std.ArrayList(Argument) {
-    var result = std.ArrayList(Argument).init(std.heap.page_allocator);
+pub fn parseArguments(allocator: *std.mem.Allocator) !std.ArrayList(Argument) {
+    var result = std.ArrayList(Argument).init(allocator);
     var argsIter = std.process.args();
     if (!argsIter.skip()) {
         @panic("launched with 0 arguments");
     }
     while (argsIter.next(stdalloc)) |rawArg| {
         const arg: [:0]u8 = rawArg catch "";
-        if (arg.len > 1 and arg[0] == '-') {
+        if (arg[0] == '-' and arg[1] == '-' and arg.len > 2) {
+            try result.append(.{ .isShort = false, .key = .{ .long = arg[2..] } });
+        } else if (arg.len > 1 and arg[0] == '-') {
             // short arg(s)
             // this could be one: -a -> a
             // or multiple: -abc -> a, b, c
@@ -33,9 +32,6 @@ fn parseArguments() !std.ArrayList(Argument) {
                 // TODO handle duplicate arguments?
                 try result.append(.{ .isShort = true, .key = .{ .short = c } });
             }
-        } else if (arg[0] == '-' and arg[1] == '-' and arg.len > 2) {
-            // long arg
-            try result.append(.{ .isShort = false, .key = .{ .long = arg[2..] } });
         } else {
             if (result.items.len == 0) {
                 std.log.err("invalid argument '{s}'.\n", .{arg});
@@ -54,21 +50,4 @@ fn parseArguments() !std.ArrayList(Argument) {
         }
     }
     return result;
-}
-
-pub fn main() !void {
-    if (parseArguments()) |arguments| {
-        for (arguments.items) |arg| {
-            if (arg.isShort) {
-                std.log.debug("got arg '{c}'", .{arg.key.short});
-            } else {
-                std.log.debug("got arg '{s}'", .{arg.key.long});
-            }
-            if (arg.hasValue) {
-                std.log.debug("\twith value '{s}'", .{arg.value});
-            }
-        }
-    } else |err| {
-        std.log.crit("invalid arguments.\n", .{});
-    }
 }
